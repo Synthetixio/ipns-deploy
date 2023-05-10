3381460290833d2f5e08cd0f46ddb625e8d5e165
Noisekit <nikita@cc.snxdao.io>
2023-05-10 11:18:10 +0800

    Add and apply Prettier

--- bin.mjs
+++ bin.mjs
@@ -1,15 +1,14 @@
 #!/usr/bin/env node
 
-import debug from "debug";
-import * as dotenv from "dotenv";
-import { promises as fs } from "fs";
-import { deploy } from "./deploy.mjs";
+import debug from 'debug';
+import * as dotenv from 'dotenv';
+import { promises as fs } from 'fs';
+import { deploy } from './deploy.mjs';
 
-dotenv.config(".env");
-const log = debug("ipns-deploy");
+dotenv.config('.env');
+const log = debug('ipns-deploy');
 
-const { IPFS_HOST, IPFS_PORT, IPFS_PROTOCOL, IPFS_USER, IPFS_PASS } =
-  process.env;
+const { IPFS_HOST, IPFS_PORT, IPFS_PROTOCOL, IPFS_USER, IPFS_PASS } = process.env;
 const [key, cid] = process.argv.slice(2);
 
 console.log(
